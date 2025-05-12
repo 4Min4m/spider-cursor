16ee9fbcaa25120606295b6a7d4a8562dbb4b5ae
Amin <36745993+4Min4m@users.noreply.github.com>
2025-05-12 17:10:13 +0000

    update files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import SpiderCursor from './SpiderCursor';
 import './App.css';
+import SpiderCursor from './SpiderCursor';
 
 function App() {
   return (
     <div className="App">
       <SpiderCursor />
       <div className="content">
-        <h1>Spider Cursor</h1>
-        <p>Move your mouse to interact with the web-like cursor!</p>
+        <h1>Interactive Spider Cursor</h1>
+        <p>Move your cursor to interact with the spiders!</p>
+        <p className="credit">Meowish</p>
       </div>
     </div>
   );
